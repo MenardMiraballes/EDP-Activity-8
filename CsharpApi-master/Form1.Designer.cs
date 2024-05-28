90b520886722436f6831bf377ed46c9d8001bfcd
Menard/BSIT-3A <menardmonreal.miraballes@bicol-u.edu.ph>
2024-05-29 02:12:55 +0800

    Added get and post for order table

--- CsharpApi-master/Form1.Designer.cs
+++ CsharpApi-master/Form1.Designer.cs
@@ -38,8 +38,22 @@
             this.label2 = new System.Windows.Forms.Label();
             this.txtUsername = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.btnPost1 = new System.Windows.Forms.Button();
+            this.txtProdnm = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtUserid = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.txtOutput1 = new System.Windows.Forms.TextBox();
+            this.btnGet1 = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtPrice = new System.Windows.Forms.TextBox();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
             this.SuspendLayout();
             // 
             // groupBox1
@@ -47,9 +61,9 @@
             this.groupBox1.Controls.Add(this.txtOutput);
             this.groupBox1.Controls.Add(this.btnGet);
             this.groupBox1.Location = new System.Drawing.Point(9, 10);
-            this.groupBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(2);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(2);
             this.groupBox1.Size = new System.Drawing.Size(403, 214);
             this.groupBox1.TabIndex = 3;
             this.groupBox1.TabStop = false;
@@ -57,7 +71,7 @@
             // txtOutput
             // 
             this.txtOutput.Location = new System.Drawing.Point(20, 17);
-            this.txtOutput.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtOutput.Margin = new System.Windows.Forms.Padding(2);
             this.txtOutput.Multiline = true;
             this.txtOutput.Name = "txtOutput";
             this.txtOutput.Size = new System.Drawing.Size(364, 110);
@@ -66,7 +80,7 @@
             // btnGet
             // 
             this.btnGet.Location = new System.Drawing.Point(20, 148);
-            this.btnGet.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnGet.Margin = new System.Windows.Forms.Padding(2);
             this.btnGet.Name = "btnGet";
             this.btnGet.Size = new System.Drawing.Size(363, 46);
             this.btnGet.TabIndex = 3;
@@ -83,9 +97,9 @@
             this.groupBox2.Controls.Add(this.txtUsername);
             this.groupBox2.Controls.Add(this.label1);
             this.groupBox2.Location = new System.Drawing.Point(10, 237);
-            this.groupBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(2);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(2);
             this.groupBox2.Size = new System.Drawing.Size(402, 199);
             this.groupBox2.TabIndex = 4;
             this.groupBox2.TabStop = false;
@@ -93,7 +107,7 @@
             // btnPost
             // 
             this.btnPost.Location = new System.Drawing.Point(19, 136);
-            this.btnPost.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnPost.Margin = new System.Windows.Forms.Padding(2);
             this.btnPost.Name = "btnPost";
             this.btnPost.Size = new System.Drawing.Size(363, 46);
             this.btnPost.TabIndex = 6;
@@ -104,7 +118,7 @@
             // txtEmail
             // 
             this.txtEmail.Location = new System.Drawing.Point(82, 82);
-            this.txtEmail.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtEmail.Margin = new System.Windows.Forms.Padding(2);
             this.txtEmail.Name = "txtEmail";
             this.txtEmail.Size = new System.Drawing.Size(300, 20);
             this.txtEmail.TabIndex = 5;
@@ -131,7 +145,7 @@
             // txtUsername
             // 
             this.txtUsername.Location = new System.Drawing.Point(82, 37);
-            this.txtUsername.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtUsername.Margin = new System.Windows.Forms.Padding(2);
             this.txtUsername.Name = "txtUsername";
             this.txtUsername.Size = new System.Drawing.Size(300, 20);
             this.txtUsername.TabIndex = 1;
@@ -146,14 +160,142 @@
             this.label1.TabIndex = 0;
             this.label1.Text = "Username:";
             // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.txtPrice);
+            this.groupBox3.Controls.Add(this.label7);
+            this.groupBox3.Controls.Add(this.btnPost1);
+            this.groupBox3.Controls.Add(this.txtProdnm);
+            this.groupBox3.Controls.Add(this.label4);
+            this.groupBox3.Controls.Add(this.label5);
+            this.groupBox3.Controls.Add(this.txtUserid);
+            this.groupBox3.Controls.Add(this.label6);
+            this.groupBox3.Location = new System.Drawing.Point(437, 237);
+            this.groupBox3.Margin = new System.Windows.Forms.Padding(2);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Padding = new System.Windows.Forms.Padding(2);
+            this.groupBox3.Size = new System.Drawing.Size(402, 199);
+            this.groupBox3.TabIndex = 6;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Enter += new System.EventHandler(this.groupBox3_Enter);
+            // 
+            // btnPost1
+            // 
+            this.btnPost1.Location = new System.Drawing.Point(19, 136);
+            this.btnPost1.Margin = new System.Windows.Forms.Padding(2);
+            this.btnPost1.Name = "btnPost1";
+            this.btnPost1.Size = new System.Drawing.Size(363, 46);
+            this.btnPost1.TabIndex = 6;
+            this.btnPost1.Text = "POST";
+            this.btnPost1.UseVisualStyleBackColor = true;
+            this.btnPost1.Click += new System.EventHandler(this.btnPost1_Click);
+            // 
+            // txtProdnm
+            // 
+            this.txtProdnm.Location = new System.Drawing.Point(82, 69);
+            this.txtProdnm.Margin = new System.Windows.Forms.Padding(2);
+            this.txtProdnm.Name = "txtProdnm";
+            this.txtProdnm.Size = new System.Drawing.Size(300, 20);
+            this.txtProdnm.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(4, 72);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(78, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Product Name:";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(16, 72);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 13);
+            this.label5.TabIndex = 2;
+            // 
+            // txtUserid
+            // 
+            this.txtUserid.Location = new System.Drawing.Point(82, 37);
+            this.txtUserid.Margin = new System.Windows.Forms.Padding(2);
+            this.txtUserid.Name = "txtUserid";
+            this.txtUserid.Size = new System.Drawing.Size(300, 20);
+            this.txtUserid.TabIndex = 1;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(16, 39);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(46, 13);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "User ID:";
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.txtOutput1);
+            this.groupBox4.Controls.Add(this.btnGet1);
+            this.groupBox4.Location = new System.Drawing.Point(436, 10);
+            this.groupBox4.Margin = new System.Windows.Forms.Padding(2);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Padding = new System.Windows.Forms.Padding(2);
+            this.groupBox4.Size = new System.Drawing.Size(403, 214);
+            this.groupBox4.TabIndex = 5;
+            this.groupBox4.TabStop = false;
+            // 
+            // txtOutput1
+            // 
+            this.txtOutput1.Location = new System.Drawing.Point(20, 17);
+            this.txtOutput1.Margin = new System.Windows.Forms.Padding(2);
+            this.txtOutput1.Multiline = true;
+            this.txtOutput1.Name = "txtOutput1";
+            this.txtOutput1.Size = new System.Drawing.Size(364, 110);
+            this.txtOutput1.TabIndex = 4;
+            // 
+            // btnGet1
+            // 
+            this.btnGet1.Location = new System.Drawing.Point(20, 148);
+            this.btnGet1.Margin = new System.Windows.Forms.Padding(2);
+            this.btnGet1.Name = "btnGet1";
+            this.btnGet1.Size = new System.Drawing.Size(363, 46);
+            this.btnGet1.TabIndex = 3;
+            this.btnGet1.Text = "GET";
+            this.btnGet1.UseVisualStyleBackColor = true;
+            this.btnGet1.Click += new System.EventHandler(this.btnGet1_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(16, 101);
+            this.label7.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(34, 13);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "Price:";
+            // 
+            // txtPrice
+            // 
+            this.txtPrice.Location = new System.Drawing.Point(82, 101);
+            this.txtPrice.Margin = new System.Windows.Forms.Padding(2);
+            this.txtPrice.Name = "txtPrice";
+            this.txtPrice.Size = new System.Drawing.Size(300, 20);
+            this.txtPrice.TabIndex = 8;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(421, 446);
+            this.ClientSize = new System.Drawing.Size(846, 446);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox4);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "C#-MYSQL API Demo";
@@ -161,6 +303,10 @@
             this.groupBox1.PerformLayout();
             this.groupBox2.ResumeLayout(false);
             this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -176,6 +322,18 @@
         private System.Windows.Forms.TextBox txtUsername;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button btnPost;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button btnPost1;
+        private System.Windows.Forms.TextBox txtProdnm;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtUserid;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.TextBox txtOutput1;
+        private System.Windows.Forms.Button btnGet1;
+        private System.Windows.Forms.TextBox txtPrice;
+        private System.Windows.Forms.Label label7;
     }
 }
 
--- CsharpApi-master/Form1.cs
+++ CsharpApi-master/Form1.cs
@@ -26,7 +26,9 @@ namespace CsharpApi
             try
             {
                 txtOutput.Clear();
-                HttpResponseMessage response = await client.GetAsync("http://localhost/api.php");
+                string url = "http://localhost/api.php?action=get_users";
+
+                HttpResponseMessage response = await client.GetAsync(url);
                 response.EnsureSuccessStatusCode();
                 string responseBody = await response.Content.ReadAsStringAsync();
                 txtOutput.Text = responseBody;
@@ -45,7 +47,7 @@ namespace CsharpApi
 
             try
             {
-                HttpResponseMessage response = await client.PostAsync("http://localhost/api.php", content);
+                HttpResponseMessage response = await client.PostAsync("http://localhost/api.php?action=create_user", content);
                 response.EnsureSuccessStatusCode();
                 string responseBody = await response.Content.ReadAsStringAsync();
                 txtOutput.Text = responseBody;
@@ -56,6 +58,59 @@ namespace CsharpApi
             }
         }
 
-       
+        private void groupBox3_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private async void btnGet1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                txtOutput1.Clear();
+                HttpResponseMessage response = await client.GetAsync("http://localhost/api.php?action=get_orders"); // Add action parameter for clarity
+                response.EnsureSuccessStatusCode();
+                string responseBody = await response.Content.ReadAsStringAsync();
+                txtOutput1.Text = responseBody;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+        private async void btnPost1_Click(object sender, EventArgs e)
+        {
+            int userId;
+            decimal price;
+
+            // Validate user input (recommended)
+            if (!int.TryParse(txtUserid.Text, out userId) || !decimal.TryParse(txtPrice.Text, out price))
+            {
+                MessageBox.Show("Invalid user ID or price format!");
+                return;
+            }
+
+            var orderData = new { user_id = userId, product_name = txtProdnm.Text, price = price };
+            string json = JsonConvert.SerializeObject(orderData);
+            HttpContent content = new StringContent(json, Encoding.UTF8, "application/json");
+
+            try
+            {
+                HttpResponseMessage response = await client.PostAsync("http://localhost/api.php?action=create_order", content);  // No action parameter needed
+                response.EnsureSuccessStatusCode();
+                string responseBody = await response.Content.ReadAsStringAsync();
+                txtOutput1.Text = responseBody;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
     }
 }
